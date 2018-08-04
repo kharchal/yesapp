c7052ed9ada99ff42ea237539c72cff9c8e44099
hav@ukr.net <githav123>
2018-08-04 20:37:26 +0300

    no jpa

--- src/main/java/hav/com/ua/yesapp/model/User.java
+++ src/main/java/hav/com/ua/yesapp/model/User.java
@@ -2,15 +2,13 @@ package hav.com.ua.yesapp.model;
 
 import lombok.Data;
 
-import javax.persistence.*;
-
 @Data
-@Entity
-@Table(name = "users")
+//@Entity
+//@Table(name = "users")
 public class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
